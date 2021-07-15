532c3b0814d938f82a09cfcf6a7e5e1c127bb3f3
Aceius <io@acei.us>
2021-07-15 10:53:59 -0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,22 @@
+const chalk = require('chalk');
+
+const printLog = (msg) => {
+    console.log(chalk.bold('['+chalk.blue('!')+']')+' '+msg)
+}
+
+const printWarning = (msg) => {
+    console.log(chalk.bold('['+chalk.yellow('!')+']')+' '+msg)
+}
+
+const printAlert = (msg) => {
+    console.log(chalk.bold('['+chalk.red('!')+']')+' '+msg)
+}
+
+const printSuccess = (msg) => {
+    console.log(chalk.bold('['+chalk.green('!')+']')+' '+msg)
+}
+
+exports.println = printLog;
+exports.warn = printWarning;
+exports.error = printAlert;
+exports.sucess = printSuccess;
